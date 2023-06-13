0f5abf9edc7408ba71f342f78bf9494806fc0e2c
Josttin Mendez <Josttin.Mendez@fundacion-jala.org>
2023-06-13 05:49:31 -0500

    Clase 1: Spaceship
    Transcripción código de la clase y tarea

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -7,45 +7,45 @@ from game.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH, SPACESHIP
 class Spaceship(Sprite):
 
     def __init__(self):
+        super().__init__()  # Llama al inicializador de la clase base (Sprite)
         self.image = pygame.transform.scale(SPACESHIP, (60, 40))
         self.rect = self.image.get_rect()
         self.rect.x = 520
         self.rect.y = 500
-    
+
     def move_left(self):
         if self.rect.left > 0:
             self.rect.x -= 10
-        
-    
+        else:
+            self.rect.x = SCREEN_WIDTH - self.rect.width
+
     def move_right(self):
         if self.rect.right < SCREEN_WIDTH:
             self.rect.x += 10
+        else:
+            self.rect.x = 0
 
     def move_up(self):
-         if self.rect.y > SCREEN_HEIGHT // 2:
+        if self.rect.y > SCREEN_HEIGHT // 2:
             self.rect.y -= 10
+        else:
+            self.rect.y = SCREEN_HEIGHT - self.rect.height
 
     def move_down(self):
-         if self.rect.y < SCREEN_HEIGHT - 10:
+        if self.rect.y < SCREEN_HEIGHT - 50:
             self.rect.y += 10
-
+        else:
+            self.rect.y = SCREEN_HEIGHT // 2
 
     def update(self, user_input):
         if user_input[pygame.K_LEFT]:
             self.move_left()
-
         elif user_input[pygame.K_RIGHT]:
             self.move_right()
-
         elif user_input[pygame.K_UP]:
             self.move_up()
-
         elif user_input[pygame.K_DOWN]:
-            self.move_down() 
+            self.move_down()
 
     def draw(self, screen):
-        screen.blit(self.image, (self.rect.x, self.rect.y))
-        
-
-
-
+        screen.blit(self.image, (self.rect.x, self.rect.y))  
