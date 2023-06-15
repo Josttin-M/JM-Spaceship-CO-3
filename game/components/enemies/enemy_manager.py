52b29577eac8bb92dff54b4b3fea7120ade43156
Josttin Mendez <Josttin.Mendez@fundacion-jala.org>
2023-06-15 01:21:36 -0500

    Clase 3: Spaceship
    Transcripción código de la clase y tarea.

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,19 +1,25 @@
 from game.components.enemies.enemy import Enemy
-from game.components.enemies.enemy_2 import Enemy_2
+from game.components.enemies.enemy2 import Enemy2
 
 
 class EnemyManager:
     def __init__(self):
         self.enemies: list[Enemy] = []
 
-    def update(self):
+    def update(self, game):
         if not self.enemies: # [] {} 0 "" -> false | [1] {1: 1} 1 -2 "a" -> true
             self.enemies.append(Enemy())
-            self.enemies.append(Enemy_2())
+            self.enemies.append(Enemy2())
 
         for enemy in self.enemies:
-            enemy.update(self.enemies)
+            enemy.update(self.enemies, game)
 
     def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
+
+    def get_enemies(self):
+        return self.enemies
+    
+    def remove_enemy(self, enemy):
+        self.enemies.remove(enemy)
--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,39 +1,19 @@
 import pygame
 from pygame.sprite import Sprite
+from game.components.bullets.bullet import Bullet
 
-from game.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH, SPACESHIP
+from game.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH, SPACESHIP,  SPACESHIP_TYPE
 
 
 class Spaceship(Sprite):
-
     def __init__(self):
-        super().__init__()  # Llama al inicializador de la clase base (Sprite)
         self.image = pygame.transform.scale(SPACESHIP, (60, 40))
         self.rect = self.image.get_rect()
         self.rect.x = 520
         self.rect.y = 500
+        self.type = SPACESHIP_TYPE
 
-    def move_left(self):
-        if self.rect.left > 0:
-            self.rect.x -= 10
-        else:
-            self.rect.x = SCREEN_WIDTH - self.rect.width
-
-    def move_right(self):
-        if self.rect.right < SCREEN_WIDTH:
-            self.rect.x += 10
-        else:
-            self.rect.x = 0
-
-    def move_up(self):
-        if self.rect.y > SCREEN_HEIGHT // 2:
-            self.rect.y -= 10
-
-    def move_down(self):
-        if self.rect.y < SCREEN_HEIGHT - 50:
-            self.rect.y += 10
-            
-    def update(self, user_input):
+    def update(self, game, user_input):
         if user_input[pygame.K_LEFT]:
             self.move_left()
         elif user_input[pygame.K_RIGHT]:
@@ -42,6 +22,30 @@ class Spaceship(Sprite):
             self.move_up()
         elif user_input[pygame.K_DOWN]:
             self.move_down()
+        elif user_input[pygame.K_SPACE]:
+            self.shoot(game.bullet_manager)
+
+    def move_left(self):
+        self.rect.x -= 10
+        if self.rect.left <= -20:
+            self.rect.x = SCREEN_WIDTH - 20
+        
+    def move_right(self):
+        self.rect.x += 10
+        if self.rect.right >= SCREEN_WIDTH + 20 :
+            self.rect.x = -20
+
+    def move_up(self):
+        if self.rect.y > SCREEN_HEIGHT // 2:
+            self.rect.y -= 10
+
+    def move_down(self):
+        if self.rect.y < SCREEN_HEIGHT - 50:
+            self.rect.y += 10
 
     def draw(self, screen):
-        screen.blit(self.image, (self.rect.x, self.rect.y))  
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    def shoot(self, bullet_manager):
+        bullet = Bullet(self)
+        bullet_manager.add_bullet(bullet)
